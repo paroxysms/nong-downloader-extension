980a515f291d946d80cac8a6a348585e5d45aa1d
Alpha <60198552+paroxysms@users.noreply.github.com>
2021-10-22 03:37:08 -0400

    Fixed YouTube downloading. **NO INSTALLER ATM**

--- src/download.rs
+++ src/download.rs
@@ -1,23 +1,22 @@
-use reqwest::Error;
 use std::process::Command;
 
 pub async fn download(link_ext: &str, link_type: &str, quality: &str, song_id: &str) {
-
     //youtube-dl.exe
     // --output %localappdata%/GeometryDash/123456789.%(ext)s
     // --extract-audio
     // --audio-quality=128k
     // --audio-format mp3
-    // https://www.youtube.com/watch?v=TxP8Wssz-W4
+    // https://www.youtube.com/watch?v=ZmsdIQuywaE
 
     let mut link = &*format!("https://www.youtube.com/watch?v={}", link_ext);
-    Command::new("converter.exe")
-        .args(&["youtube-dl.exe",
+    Command::new("nong/converter.exe")
+        .args(&[
             format!("--output %localappdata%/GeometryDash/{}.%(ext)s", song_id).as_str(),
             "--extract-audio",
             format!("--audio-quality={}]", quality).as_str(),
             "--audio-format mp3",
-            link])
+            link
+        ])
         .output()
         .expect("failed to execute process");
 }
